4298fff1cdd575a27275979a38652679c5dbcc56
minsu3007 <minsu3007@gmail.com>
2022-11-23 22:20:31 +0900

    Queue구현

---
+++ 자료구조/Circular buffer.playground/Contents.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+struct CircularBuffer<T> {
+    var data: [T]
+    var head: Int = 0, tail: Int = 0
+    var count: Int = 0
+    
+    func push() {
+    }
+    
+    func pop() {
+    }
+    
+    func peek() {
+    }
+    
+    func isEmpty() {
+    }
+    
+    func isFull() {
+    }
+    
+    func clear() {
+    }
+}
