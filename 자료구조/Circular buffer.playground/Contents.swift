c6f742f10412f3b841fd115ceb4af1ebbff8fe27
minsu3007 <minsu3007@gmail.com>
2022-11-27 16:46:26 +0900

    순환버퍼&우선순위큐 구현

--- 자료구조/Circular buffer.playground/Contents.swift
+++ 자료구조/Circular buffer.playground/Contents.swift
@@ -1,25 +1,110 @@
 import Foundation
 
+private struct Constants {
+    fileprivate static let defaultBufferCapacity: Int = 16
+}
+
+public enum CircularBufferOperation {
+    case Ignore, Overwrite
+}
+
 struct CircularBuffer<T> {
-    var data: [T]
-    var head: Int = 0, tail: Int = 0
-    var count: Int = 0
+    fileprivate var data: [T]
+    fileprivate var head: Int = 0, tail: Int = 0
     
-    func push() {
+    private var internalCount: Int = 0
+    public var count: Int {
+        return self.internalCount
     }
     
-    func pop() {
+    private var overwriteOperation: CircularBufferOperation = .Overwrite
+    
+    public init() {
+        self.data = [T]()
+        self.data.reserveCapacity(Constants.defaultBufferCapacity)
     }
     
-    func peek() {
+    public init(_ count: Int, overwriteOperation: CircularBufferOperation = .Overwrite) {
+        var capacity = count
+        
+        if capacity < 1 {
+            capacity = Constants.defaultBufferCapacity
+        }
+        
+        //count가 2의 거듭제곱이 아닌 경우 가장 가까운 수를 거듭제곱해서 할당 해줌
+        if ((capacity & (~capacity + 1)) != capacity) {
+            var binary = 1
+            while (binary < capacity) {
+                binary = binary << 1
+            }
+            
+            capacity = binary
+        }
+        
+        self.data = [T]()
+        self.data.reserveCapacity(capacity)
+        self.overwriteOperation = overwriteOperation
     }
     
-    func isEmpty() {
+    public func isEmpty() -> Bool {
+        return (self.count < 1)
     }
     
-    func isFull() {
+    public mutating func pop() -> T? {
+        if isEmpty() {
+            return nil
+        }
+        
+        let el = self.data[head]
+        head = incrementPointer(pointer: head)
+        internalCount -= 1
+        
+        return el
     }
     
-    func clear() {
+    public func isFull() -> Bool {
+        return self.count == self.data.capacity
+    }
+    
+    fileprivate func incrementPointer(pointer: Int) -> Int {
+        return (pointer + 1) & (self.data.capacity - 1)
+    }
+    
+    fileprivate func decrementPointer(pointer: Int) -> Int {
+        return (pointer - 1) & (self.data.capacity - 1)
+    }
+    
+    public func peek() -> T? {
+        if isEmpty() {
+            return nil
+        }
+        
+        return self.data[head]
+    }
+    
+    public mutating func push(element: T) {
+        if isFull() {
+            switch overwriteOperation {
+            case .Ignore: return
+            case .Overwrite: pop()
+            }
+        }
+        
+        if self.data.endIndex < self.data.capacity {
+            self.data.append(element)
+        } else {
+            self.data[tail] = element
+        }
+        
+        self.tail = incrementPointer(pointer: tail)
+        self.internalCount += 1
+    }
+    
+    public var capacity: Int {
+        get {
+            return self.data.capacity
+        } set {
+            self.data.reserveCapacity(newValue)
+        }
     }
 }
